bbbcdeaead3c82b5df5371ce9b98d9dc94f9eb0e
Alex <ZimenkovIUAI@gmail.com>
2020-05-22 01:05:10 +0300

    Начал делать задание

--- js/map.js
+++ js/map.js
@@ -1,9 +1,31 @@
 'use strict';
 
+var DIMENSIONS_MAP_PIN_IMG = {
+  x: 62,
+  y: 62,
+  after: 22
+};
+
+//Функция Ранодом
 function randomInteger(min, max) {
     var rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
   }
+
+
+  //Поск позиции элемента
+  function getCoords(elem) {
+    var box = elem.getBoundingClientRect();
+  
+    return {
+      top: box.top + pageYOffset,
+      bottom : box.bottom  + pageYOffset,
+      left: box.left + pageXOffset,
+      right: box.right  + pageXOffset
+    };
+  }
+
+
 
 var arrApartmentDescriptions = ["Большая уютная квартира", "Маленькая неуютная квартира",
 "Огромный прекрасный дворец", "Маленький ужасный дворец", "Красивый гостевой домик",
@@ -14,6 +36,7 @@ var arrFeatures = [" wifi", " dishwasher", " parking", " washer", " elevator", "
 // var locationX = document.querySelector('.map__pinsoverlay');
 var locationX = [-500, 500];
 
+//Функция добавления случайного элемента из массива
 function randomFeatures (array) {
   var arr = [];
   for (var i = 0; i<array.length; i++){
@@ -25,6 +48,7 @@ function randomFeatures (array) {
   return arr
 }
 
+//Функция создания рандомного массива
 function createArray (numberObjects){
   var arrayObjects = [];
   for (var i = 0; i<numberObjects; i++){
@@ -47,7 +71,7 @@ function createArray (numberObjects){
         "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"],
         location: {
           // x: randomInteger(locationX.offsetLeft,locationX.offsetWidth),
-          x: randomInteger(locationX[0],locationX[1]),
+          x: randomInteger(50,1150),
           y: randomInteger(130,630)
       }
     }
@@ -57,35 +81,33 @@ function createArray (numberObjects){
       return arrayObjects;
   }
 
-var similarAds = createArray(1);
+var similarAds = createArray(2);
 
 var map = document.querySelector('.map');
-map.classList.remove('map--faded');
-
 var btnTemplate = document.querySelector('template').content.querySelectorAll('button')[1];
 
 function createIcon (){
-  var pool = document.querySelector('.map__pin');
+  var div = document.createElement('div');
   for (var i = 0; i<similarAds.length; i++){
       var element = btnTemplate.cloneNode(true);
+      div.classList.add('map__pins')
       element.setAttribute('style', 'left:'+ similarAds[i].offer.location.x + 'px');
-      element.setAttribute('top', similarAds[i].offer.location.y + 'px');
+      element.style.top = similarAds[i].offer.location.y + 'px';
       element.querySelector('img').setAttribute('src', similarAds[i].author.avatar);
       element.setAttribute('alt', similarAds[i].offer.title);
-      pool.appendChild(element);
+      element.classList.add ('elem');
+      div.appendChild(element);
   }
-  return pool;
+  return div;
 }
-var iconBtn = createIcon();
-var mapPins = document.querySelector('.map__pins');
-mapPins.appendChild(iconBtn)
 
 var artTemplate = document.querySelector('template').content.querySelector('article');
 
 function createArticle (){
   var pool = document.querySelector('.map__filters-container');
+  // var elem = document.querySelectorAll('.elem');
   for (var i = 0; i<similarAds.length; i++){
-      var element = artTemplate.cloneNode(true);
+    var element = artTemplate.cloneNode(true);
       element.querySelector('h3').textContent = similarAds[i].offer.title;
       element.querySelector('h3').classList.add('popup__title');
       element.querySelectorAll('p')[0].textContent = similarAds[i].offer.address;
@@ -114,16 +136,61 @@ function createArticle (){
         element.querySelectorAll('p')[4].textContent = similarAds[i].offer.description;
         element.querySelectorAll('p')[4].classList.add('popup__description');
         element.querySelectorAll('img')[0].setAttribute('src', similarAds[i].author.avatar);
+        element.setAttribute('disabled', 'disabled');
         
         
         // element.querySelectorAll('img')[1].setAttribute('src', similarAds[i].offer.photos[0]);
-        
-
       pool.before(element);
-  }
+    }
   return pool;
 }
-var article = createArticle();
+var createdAd = createArticle();
 
-var pool123 = document.querySelector('.map');
-console.log(pool123);
+var fieldset = document.querySelectorAll('fieldset');
+var address = document.querySelector('#address');
+
+var addDisabled = function(element){
+  for (var i = 0; i < element.length; i++){
+    element[i].setAttribute('disabled', 'disabled');
+  }
+};
+
+var removeDisabled = function(element){
+  for (var i = 0; i < element.length; i++){
+    element[i].removeAttribute('disabled');
+  }
+};
+var fieldsetDisabled = addDisabled(fieldset);
+
+
+
+var btnMap = document.querySelector('.map__pin--main');
+var mapPins = document.querySelector('.map__pins');
+var iconBtn = createIcon();
+
+btnMap.addEventListener('mouseup', function(){
+  fieldsetDisabled = removeDisabled(fieldset);
+  map.classList.remove('map--faded');
+  address.setAttribute('placeholder', (getCoords(btnMap).right-(DIMENSIONS_MAP_PIN_IMG.x/2)) +
+  ':' + (getCoords(btnMap).top-DIMENSIONS_MAP_PIN_IMG.y-DIMENSIONS_MAP_PIN_IMG.after));
+  mapPins.appendChild(iconBtn)
+
+});  
+
+// var elem = document.querySelectorAll('.elem'); 
+
+iconBtn.addEventListener('click', function(e){
+  // var target = e.target;
+
+}) 
+
+
+
+// var btnClose = document.querySelector('.popup__close');
+// console.log(btnClose);
+
+
+// btnClose.addEventListener('click', function(){
+//   btnClose.hidden = true;
+
+// })
