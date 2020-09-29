4d032c28665987dd2e88bc46c5ef8d75212f8ccc
Alex <ZimenkovIUAI@gmail.com>
2020-09-29 23:34:50 +0300

    add render method for class

--- src/modules/script.js
+++ src/modules/script.js
@@ -1,14 +1,13 @@
 'use strict';
 
 //Данные для гененрации карточек   
-const umberCards = 1,
-	  titleTemplate = ["Большая уютная квартира", "Маленькая неуютная квартира",
+const titleTemplate = ["Большая уютная квартира", "Маленькая неуютная квартира",
 					   "Огромный прекрасный дворец", "Маленький ужасный дворец", "Красивый гостевой домик",
 					   "Некрасивый негостеприимный домик", "Уютное бунгало далеко от моря",
 					   "Неуютное бунгало по колено в воде"],
 	  typeTemplate = ['palace', 'flat', 'house', 'bungalo'],
 	  checkTemplate = ['12:00', '13:00', '14:00'],
-	  featuresTemplate = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner',' '],
+	  featuresTemplate = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
 	  photosTemplate = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg",
 					    "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
 						"http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
@@ -17,88 +16,84 @@ function getRandomNumber(min, max) {
 }
 // // // //
 
-const mapWrapper = document.querySelector('.map.map--faded');
 
-let cards = createArrCard(8);
-	  
-
-function createArrCard (umberCards) {
-	let arr = [];
-	for (let i = 0; i < umberCards; i++){
-		arr[i] = {
-			'author': {'avatar':`img/avatars/user0${i+1}.png`},
-			'location': {
-				'x': mapWrapper.offsetWidth,
-				'y': getRandomNumber(130, 630)
-			},
-			'offer': {
-				'title': titleTemplate[getRandomNumber(0, titleTemplate.length - 1)],
-				'address': `${location.x}, ${location.y}`,
-				'price': getRandomNumber(1000, 1000000),
-				'type': typeTemplate[getRandomNumber(0, typeTemplate.length - 1)],
-				'rooms': getRandomNumber(1, 5),
-				'guests': getRandomNumber(0, 6),
-				'checkin': checkTemplate[getRandomNumber(0, checkTemplate.length - 1)],
-				'checkout': checkTemplate[getRandomNumber(0, checkTemplate.length - 1)],
-				'features': featuresTemplate[getRandomNumber(0, featuresTemplate.length - 1)],
-				'description': ' ',
-				'photos': photosTemplate,  
-			},
-		};
-	}
-	return arr;
-}
-
-
-
-
-
-
-
-
-
-
+const mapWrapper = document.querySelector('#map-wrapper');
 
 
 // ?Способ создания с помощью класса
-// class AnnouncementCard {
-// 	constructor(avatar, title, price, type, rooms, guests, checkin, checkout,
-// 		features, description, photos, locationX, locationY){
-// 		this.author = {'avatar': `img/avatars/user${avatar}.png`};
-// 		this.offer = {
-// 			'title': title,
-// 			'address': `${locationX}, ${locationY}`,
-// 			'price': price,
-// 			'type': type,
-// 			'rooms': rooms,
-// 			'guests': guests,
-// 			'checkin': checkin,
-// 			'checkout': checkout,
-// 			'features': features,
-// 			'description': description,
-// 			'photos': photos
-// 		};
-// 		this.location = {
-// 			'x': locationX,
-// 			'y': locationY
-// 		};
-// 	}
-// }
+class AnnouncementCard {
+	constructor(avatar, title, price, type, rooms, guests, checkin, checkout,
+		features, description, photos, locationX, locationY){
+		this.author = {'avatar': `img/avatars/user${avatar}.png`};
+		this.offer = {
+			'title': title,
+			'address': `${locationX}, ${locationY}`,
+			'price': price,
+			'type': type,
+			'rooms': rooms,
+			'guests': guests,
+			'checkin': checkin,
+			'checkout': checkout,
+			'features': features,
+			'description': description,
+			'photos': photos
+		};
+		this.location = {
+			'x': locationX,
+			'y': locationY
+		};
+	}
 
-// function createCard (umberCards) {
-// 	for (let i = 0; i < umberCards; i++){
-// 		 cards[i] = new AnnouncementCard(
-// 			'0'+(i+1),
-// 			titleTemplate[getRandomNumber(0, titleTemplate.length - 1)],
-// 			getRandomNumber(1000, 1000000),
-// 			typeTemplate[getRandomNumber(0, typeTemplate.length - 1)],
-// 			getRandomNumber(1, 5),
-// 			getRandomNumber(0, 6),
-// 			checkTemplate[getRandomNumber(0, checkTemplate.length - 1)],
-// 			checkTemplate[getRandomNumber(0, checkTemplate.length - 1)],
-// 			featuresTemplate[getRandomNumber(0, featuresTemplate.length - 1)],
-// 			' ',
-// 			photosTemplate, mapWrapper.offsetWidth, getRandomNumber(130, 630)
-// 		);
-// 	}
-// }
+	render() {
+		const {avatar} = this.author,
+		{title, price, type, rooms, guests, checkin, checkout,
+		features, description, photos} = this.offer,
+		{y, x} = this.location;
+
+		const div = document.createElement('div');
+		div.innerHTML = `
+			<img src=${avatar} class="popup__avatar" width="70" height="70">
+			<button class="popup__close">Закрыть</button>
+			<h3>${title}</h3>
+			<p><small>${x} Tōkyō-to, Chiyoda-ku, Ichibanchō, ${y}</small></p>
+			<p class="popup__price">${price} &#x20bd;/ночь</p>
+			<h4>${type}</h4>
+			<p>${rooms} комнаты для ${guests} гостей</p>
+			<p>Заезд после ${checkin}, выезд до ${checkout}</p>
+			<ul class="popup__features">
+			  <li class="feature feature--${features}"></li>
+			</ul>
+			<p>${description} (тут будет отзыв)</p>
+			<ul class="popup__pictures">
+			  <li><img src=${photos[0]}></li>
+			</ul>
+		`;
+		div.classList.add('map__card');
+		document.querySelector('.map__pins').append(div);
+	}
+}
+
+let cards = createCard(8);
+// cards.forEach(item => {item.render();});
+
+function createCard (umberCards) {
+	let arr = [];
+	for (let i = 0; i < umberCards; i++){
+		arr[i] = new AnnouncementCard(
+			'0'+(i+1),
+			titleTemplate[getRandomNumber(0, titleTemplate.length - 1)],
+			getRandomNumber(1000, 1000000),
+			typeTemplate[getRandomNumber(0, typeTemplate.length - 1)],
+			getRandomNumber(1, 5),
+			getRandomNumber(0, 3),
+			checkTemplate[getRandomNumber(0, checkTemplate.length - 1)],
+			checkTemplate[getRandomNumber(0, checkTemplate.length - 1)],
+			featuresTemplate[getRandomNumber(0, featuresTemplate.length - 1)],
+			' ',
+			photosTemplate,
+			mapWrapper.offsetWidth,
+			getRandomNumber(130, 630)
+		);
+	}
+	return arr;
+}
